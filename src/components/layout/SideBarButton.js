59898824cbd2628f9a27cc6605023232cac10fb0
CodeAgony <yaroslav.bkh@gmail.com>
2019-08-20 12:00:27 +0300

    Fix parameter name to fit react

--- src/components/layout/SideBarButton.js
+++ src/components/layout/SideBarButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SideBarButton = () => {
   return (
     <div className="sidebar-button">
-      <i class="fas fa-lg fa-search-location"></i>
+      <i className="fas fa-lg fa-search-location"></i>
     </div>
   );
 };
