127324ef59d277c2dee49b964d836ac0819d918d
CodeAgony <yaroslav.bkh@gmail.com>
2019-08-20 11:54:58 +0300

    Add styled sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import MapContainer from './components/map/MapContainer';
 import SideBarButton from './components/layout/SideBarButton';
+import SideBar from './components/sidebar/SideBar';
 
 function App() {
   return (
     <div className="App">
       <SideBarButton />
+      <SideBar />
       <MapContainer />
     </div>
   );
---
+++ src/components/sidebar/SideBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Search from './Search';
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <h3>Vinnytsia</h3>
+      <Search />
+      <p>test</p>
+    </div>
+  );
+};
+
+export default SideBar;
