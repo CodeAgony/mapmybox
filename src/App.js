49364a901a93ef827226fa8e21448d823ea2ebd1
CodeAgony <yaroslav.bkh@gmail.com>
2019-08-21 15:41:17 +0300

    Put an iife in useEffect as suggested

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ function App() {
   const [isVisible, setVisibility] = useState(false);
   const [places, setPlaces] = useState(null);
 
-  useEffect(async () => {
-    const res = await axios(
-      'https://api.mapbox.com/geocoding/v5/mapbox.places/restaurant.json?proximity=28.4685,%2049.2344&bbox=27.37442,48.06551,30.023758,49.889038&limit=10&access_token=pk.eyJ1IjoiY29kZWFnb255IiwiYSI6ImNqemg5NHE4dDBiMGczbG16cDhyb3Q1NTYifQ.h_WYdEWMtkuogpC9_l89SQ'
-    );
+  useEffect(() => {
+    (async () => {
+      const res = await axios(
+        'https://api.mapbox.com/geocoding/v5/mapbox.places/restaurant.json?proximity=28.4685,%2049.2344&bbox=27.37442,48.06551,30.023758,49.889038&limit=10&access_token=pk.eyJ1IjoiY29kZWFnb255IiwiYSI6ImNqemg5NHE4dDBiMGczbG16cDhyb3Q1NTYifQ.h_WYdEWMtkuogpC9_l89SQ'
+      );
 
-    setPlaces(res.data);
+      setPlaces(res.data);
+    })();
   }, []);
 
   return (
